4f4798c100a718bdf890a4228307930e9f4c8b77
Abdullah <abdullah_busari@yahoo.com>
2019-09-26 14:36:24 +0100

    'new_setup'

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for, flash, redirect
+from app.forms import RegistrationForm, LoginForm
+
 
 @app.route("/")
 @app.route("/home")
@@ -13,3 +15,22 @@ def about():
 @app.route("/calculate")
 def calculate():
     return render_template('calculate.html', title='Calculate')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'username' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
