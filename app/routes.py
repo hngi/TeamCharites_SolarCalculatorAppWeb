ace0ea61f5c93e48e3cce825f4b7794f78aaba76
Busari Abdullah <abdullah_busari@yahoo.com>
2019-09-28 13:53:20 +0100

    deploy

---
+++ app/routes.py
@@ -0,0 +1,45 @@
+from app import app
+from flask import render_template, url_for, flash, redirect
+from app.forms import RegistrationForm, LoginForm
+
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/calculate")
+def calculate():
+    return render_template('calculate.html', title='Calculate')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'username' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def error_404(error):
+    return render_template('404.html'), 404
