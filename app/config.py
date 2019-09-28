47607f590cacdd76e442d4a097464c259ed61eaa
Horllameeday <45775920+Horllameeday@users.noreply.github.com>
2019-09-28 01:45:53 +0100

    Delete config.py

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from app import routes
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '.env'))
-
-class Config(object):
-    SECRET_KEY = 'horlla5791628be0c676dfde280ba245'
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
