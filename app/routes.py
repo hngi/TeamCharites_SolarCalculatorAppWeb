a22bf54c2d01f79742c1883f574174f1a3c34159
Busari Abdullah <abdullah_busari@yahoo.com>
2019-09-28 14:55:52 +0100

    deploy1

--- app/routes.py
+++ app/routes.py
@@ -43,3 +43,11 @@ def logout():
 @app.errorhandler(404)
 def error_404(error):
     return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def error_403(error):
+    return render_template('404.html'), 403
+
+@app.errorhandler(500)
+def error_500(error):
+    return render_template('404.html'), 500
