8711831db9b9cf9ccf193650e464cd7556be6e7f
Tinuade Adeleke <summittinuade@gmail.com>
2019-09-28 15:23:07 +0100

    updates

--- app/routes.py
+++ app/routes.py
@@ -21,65 +21,65 @@ def calculate():
     return render_template('calculate.html', title='Calculate')
 
 
-@app.route('/power_consumption', method='POST')
-def total_output_load():
-    output = []
-    data = request.get_json()
-    for item in data:
-        app_qtty = item['qtty']
-        app_hours = item['hours']
-        app_consumption = item['consumption']
-        app_output = app_qtty * app_hours * app_consumption
-        output.append(app_output)
-    total_app_output = adder(output)
-    result = {'status': 'oK', 'status_code': '200', 'total_power_consumption': total_app_output}
-    return jsonify(result)
-
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-        flash('Your account has been created! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='Register', form=form)
-
-
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user)
-            next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('index'))
-        else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
-
-
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.errorhandler(404)
-def error_404(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(403)
-def error_403(error):
-    return render_template('404.html'), 403
-
-@app.errorhandler(500)
-def error_500(error):
-    return render_template('404.html'), 500
+# @app.route('/power_consumption', methods=['POST'])
+# def total_output_load():
+#     output = []
+#     data = request.get_json()
+#     for item in data:
+#         app_qtty = item['qtty']
+#         app_hours = item['hours']
+#         app_consumption = item['consumption']
+#         app_output = app_qtty * app_hours * app_consumption
+#         output.append(app_output)
+#     total_app_output = adder(output)
+#     result = {'status': 'oK', 'status_code': '200', 'total_power_consumption': total_app_output}
+#     return jsonify(result)
+#
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+#         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Your account has been created! You are now able to log in', 'success')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
+#
+#
+# @app.route("/login", methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user and bcrypt.check_password_hash(user.password, form.password.data):
+#             login_user(user)
+#             next_page = request.args.get('next')
+#             return redirect(next_page) if next_page else redirect(url_for('index'))
+#         else:
+#             flash('Login Unsuccessful. Please check username and password', 'danger')
+#     return render_template('login.html', title='Login', form=form)
+#
+#
+# @app.route("/logout")
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
+#
+#
+# @app.errorhandler(404)
+# def error_404(error):
+#     return render_template('404.html'), 404
+#
+# @app.errorhandler(403)
+# def error_403(error):
+#     return render_template('404.html'), 403
+#
+# @app.errorhandler(500)
+# def error_500(error):
+#     return render_template('404.html'), 500
