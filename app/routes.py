1d6897ecd33abfafa8beba1d130b2d01954f20c3
Tinuade Adeleke <summittinuade@gmail.com>
2019-09-29 14:31:03 +0100

    result fix

--- app/routes.py
+++ app/routes.py
@@ -21,6 +21,7 @@ def about():
 def calculate():
     return render_template('calculate.html', title='Calculate')
 
+
 @app.route("/get_result")
 def get_result():
     return render_template('get_result.html')
