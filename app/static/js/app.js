dd88bf190da2ec40fa453f2ea941eab276198fa1
Tinuade Adeleke <summittinuade@gmail.com>
2019-09-29 15:09:18 +0100

    result fix

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -6,7 +6,7 @@ $("document").ready(function(){
         var days_of_autonomy = $("#doa").val();
         $.ajax({
             url: "https://charites-solarcalculator.herokuapp.com/result/",
-            type: "POST",
+            type: "GET",
             contentType: "application/json",
             data: JSON.stringify({"total_power": total_power, "solar_panel_power":solar_panel_power,
             "battery_voltage":battery_voltage, "days_of_autonomy":days_of_autonomy})
