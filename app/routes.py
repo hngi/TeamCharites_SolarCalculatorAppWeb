fcd5f850de89130545f866628d6435d4141c2443
Tinuade Adeleke <summittinuade@gmail.com>
2019-09-29 16:21:13 +0100

    login fix

--- app/routes.py
+++ app/routes.py
@@ -90,8 +90,7 @@ def login():
     user = User.query.filter_by(email=data['username']).first()
     if user and bcrypt.check_password_hash(user.password, form.password.data):
         login_user(user)
-        next_page = request.args.get('next')
-        return redirect(next_page) if next_page else redirect(url_for('index'))
+        return redirect(url_for('index'))
     return flash('Login Unsuccessful. Please check username and password', 'danger')
 
 
