ac7b272e7465716f3208797f5f618c2d42fbd381
Tinuade Adeleke <summittinuade@gmail.com>
2019-09-29 07:38:32 +0100

    fixes

--- app/routes.py
+++ app/routes.py
@@ -41,19 +41,19 @@ def result():
 #
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('index'))
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
-    # if form.validate_on_submit():
-    data = request.form
-    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    if form.validate_on_submit():
+        data = request.form
+        hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
         # hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-    user = User(username=data['username'], email=data['email'],password=hashed_password)
+        user = User(username=data['username'], email=data['email'],password=hashed_password)
         # user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-    db.session.add(user)
-    db.session.commit()
-    flash('Your account has been created! You are now able to log in', 'success')
-        # return redirect(url_for('login'))
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
