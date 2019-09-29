b196710d9846dee8c0ceba12058508102558781e
Tinuade Adeleke <summittinuade@gmail.com>
2019-09-29 16:09:17 +0100

    login fix

--- app/routes.py
+++ app/routes.py
@@ -93,8 +93,8 @@ def login():
         next_page = request.args.get('next')
         return redirect(next_page) if next_page else redirect(url_for('index'))
     else:
-        flash('Login Unsuccessful. Please check username and password', 'danger')
-    return flash('Login Successful!')
+        return flash('Login Unsuccessful. Please check username and password', 'danger')
+
 
 
 @app.route("/logout")
