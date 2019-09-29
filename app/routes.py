671534e05d6e9d09efaa5295401a4310ff7a65e8
Tinuade Adeleke <summittinuade@gmail.com>
2019-09-29 14:04:09 +0100

    result fix

--- app/routes.py
+++ app/routes.py
@@ -96,14 +96,14 @@ def login():
 #     return redirect(url_for('index'))
 
 
-@app.errorhandler(404)
-def error_404(error):
-    return render_template('404.html'), 404
+# @app.errorhandler(404)
+# def error_404(error):
+#     return render_template('404.html'), 404
 #
-@app.errorhandler(403)
-def error_403(error):
-    return render_template('404.html'), 403
+# @app.errorhandler(403)
+# def error_403(error):
+#     return render_template('404.html'), 403
 #
-@app.errorhandler(500)
-def error_500(error):
-    return render_template('404.html'), 500
+# @app.errorhandler(500)
+# def error_500(error):
+#     return render_template('404.html'), 500
