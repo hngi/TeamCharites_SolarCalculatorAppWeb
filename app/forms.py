916ec93228112249d938881035660b878ff2a616
Busari Abdullah <abdullah_busari@yahoo.com>
2019-10-03 22:58:51 +0100

    committed

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, IntegerField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
+import re
 
 
 class RegistrationForm(FlaskForm):
@@ -15,14 +16,26 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
+        username = username.data
+        user = User.query.filter_by(username=username).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
+        elif len(username) < 5:
+            raise ValidationError('Username must be at least 5 characters long')
+        elif re.match(r'[A-Za-z0-9_]', username):
+            raise ValidationError('Username should be a combination of lowercase and uppercase alphabetic characters, numeric characters and underscore')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('That email is taken. Please choose a different one.')
+
+    def validate_password(self, password):
+        password = password.data
+        if re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', password):
+            pass
+        else:
+            raise ValidationError('Password must be more than 8 characters long and must contain uppercase alphabetic character(s), lowercase alphabetic character(s), numeric character(s) and symbol(s)')
 
 class LoginForm(FlaskForm):
     login_data = StringField('Username/Email',
